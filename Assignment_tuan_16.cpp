8faaa9e1b16b21c1a4ed617968134178d3487322
Ngh1aSoLazy <149345707+Ngh1aSoLazy@users.noreply.github.com>
2024-12-22 12:19:12 +0700

    Done!

--- Assignment_tuan_16.cpp
+++ Assignment_tuan_16.cpp
@@ -36,34 +36,78 @@ NODE* MAKE_NODE(int data)
     return p;
 }
 
+// Hàm kiểm tra chiều cao của cây
+int height(NODE* root)
+{
+    if(root == NULL) return -1;
+    else
+    {
+        int lheight = height(root->left);
+        int rheight = height(root->right);
+        if(lheight > rheight) return (lheight + 1);
+        else return (rheight + 1);
+    }
+}
+
+// Hàm kiểm tra hệ số cân bằng
+int BAL_FACTOR(NODE* root)
+{
+    if(root == NULL) return -1;
+    else return (height(root->left) - height(root->right)); 
+}
+
+// Hàm xoay đơn, kép
+NODE* LL(NODE* root) // Left-left imbalance
+{
+    NODE* p = root->left;
+    NODE* p2 = p->right;
+
+    p->right = root;
+    root->left = p2;
+
+    return p;
+}
+
+NODE* RR(NODE* root) // Right-right imbalance
+{
+    NODE* p = root->right;
+    NODE* p2 = p->left;
+
+    p->left = root;
+    root->right = p2;
+
+    return p;
+}
+
+NODE* LR(NODE* root) // Left-right imbalance
+{
+    RR(root->left);
+    LL(root);
+
+    return root;
+}
+
+NODE* RL(NODE* root) // Right-left imbalance
+{
+    LL(root->right);
+    RR(root);
+
+    return root;
+}
+
 // Hàm insert
 NODE* INSERT(NODE* root, int data)
 {
     if(root == NULL) return MAKE_NODE(data);
     else if(data <= root->data) root->left = INSERT(root->left, data);
     else if(data > root->data)  root->right = INSERT(root->right, data);
-    
+
+    if(BAL_FACTOR(root) > 1 && (BAL_FACTOR(root->left) == 1 || BAL_FACTOR(root->left) == 0)) return LL(root);
+    if(BAL_FACTOR(root) > 1 && BAL_FACTOR(root->left) < 0) return LR(root);
+    if(BAL_FACTOR(root) < -1 && (BAL_FACTOR(root->right) == 1 || BAL_FACTOR(root->right) == 0)) return RL(root);
+    if(BAL_FACTOR(root) < -1 && BAL_FACTOR(root->left) < 0) return RR(root);
+
     return root;
-}
-
-// Hàm xoay đơn, kép
-void LL(NODE* root)
-{
-    
-}
-
-// Hàm kiểm tra cân bằng
-int IS_BALANCE(NODE* root, int &bal_factor)
-{
-    int hl, hr;
-
-    bal_factor = hl - hr;
-
-    
-    if(bal_factor == -1 || bal_factor == 0 || bal_factor == 1) return 1;
-    else return 0;
-
-
 }
 
 // Duyệt cây theo kiểu tiền tố (gốc - trái - phải)
@@ -88,9 +132,8 @@ int main()
         root = INSERT(root, data); // Hàm nhập
     }
 
-    // Hàm in cây ra theo kiểu tiền tốtố
+    // Hàm in cây ra theo kiểu tiền tố
     PREFIX(root);
-
 
     return 0;
 }
